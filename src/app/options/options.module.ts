9e0c4e91834a889f63cf3882e6a32b32bd60e299
Jan Verhoeckx <janverhoeckx@gmail.com>
2021-07-29 22:55:30 +0200

    - Fix options module - Change material theme

--- src/app/options/options.module.ts
+++ src/app/options/options.module.ts
@@ -8,11 +8,10 @@ import {MatDividerModule} from '@angular/material/divider';
 import {AddTemplateComponent} from './components/add-template/add-template.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { SaveSuccessfullComponent } from './components/snackbar/save-successfull/save-successfull.component';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
-  declarations: [OptionsComponent, AddTemplateComponent, SaveSuccessfullComponent],
+  declarations: [OptionsComponent, AddTemplateComponent],
   imports: [
     CommonModule,
     OptionsRoutingModule,
